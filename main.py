5d0dcd93f4359eeb9cc491859815d4bfdaa3fc9b
R. C. Björn H <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-09-15 19:04:25 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Rainer C. B. Herold
+# Version 0.1 02.01.2021
+# Version 1.0 04.01.2021
+# Version 1.1 24.01.2021
+# Version 1.2 12.09.2022
+
+# Libraries
+from VF import *
+
+# Main
+if __name__ == '__main__':
+    GUI()
